5f1505a9dbaef7fccb1e326d9b3e13c11a5ccf4c
relam10 <alejandro2210lp@gmail.com>
2021-06-04 12:30:24 +0200

    Creada funcionalidad de javascript

---
+++ index.js
@@ -0,0 +1,37 @@
+const secondsDiv = document.querySelector('#seconds')
+const minutesDiv = document.querySelector('#min')
+const hoursDiv = document.querySelector('#hours')
+const daysDiv  = document.querySelector('#days')
+const inputDate = document.querySelector('.goal-date')
+
+
+
+
+
+const countDown = () => {
+    const goalDate = '1 Jan 2022'
+
+    const desiredGoalDate = new Date(goalDate)
+    const currentDate = new Date()
+
+    const totalSeconds = ((desiredGoalDate - currentDate) / 1000)
+
+    const days = Math.floor(totalSeconds / 3600 / 24 );
+    const hours = Math.floor(totalSeconds /3600) % 24 
+    const minutes = Math.floor(totalSeconds / 60) % 24 
+    const seconds = Math.floor (totalSeconds % 24)
+
+    daysDiv.textContent = days
+    hoursDiv.textContent = hours
+    secondsDiv.textContent = seconds
+    minutesDiv.textContent = minutes
+    console.log(goalDate);
+}
+
+// Initial Call
+countDown()
+
+setInterval(countDown , 1000)
+
+
+
